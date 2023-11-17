4449e34dfc9604c8e9b45a829b7c79058fa59bdb
om0g1 <omogidavis@gmail.com>
2023-11-18 01:22:18 +0300

    softbody#2

--- simulations/softbody/main.js
+++ simulations/softbody/main.js
@@ -7,7 +7,7 @@ const softbodyWorld = yapheEngine.createWorld2D();
 const softbody = softbodyWorld.createSoftBody();
 softbody.spacing = 100;
 softbody.stiffness = 0.15;
-softbody.createGeometry(4, 4);
+softbody.createGeometry(5, 5);
 softbody.borderSprings.forEach((spring) => {
     spring.drawStyle.color = "red";
 })
